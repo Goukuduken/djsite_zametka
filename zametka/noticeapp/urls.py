c07b7bb52e946d2502efddd53d79ca53ba783f6e
Timofey Plotnikov <goprozero80@gmail.com>
2022-09-01 03:17:47 +0500

    small changes: добавлены ссылки, пока без привязки БД

--- zametka/noticeapp/urls.py
+++ zametka/noticeapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index), #доменсайта, + префикс noticeapp
-    path('opennotice/<int:notice_id>/', fullNotice)
+    path('', index, name='home'), #доменсайта, + префикс noticeapp
+    path('fullnotice/', fullNotice, name='fullnotice')
 ]
 
--- zametka/noticeapp/views.py
+++ zametka/noticeapp/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render, redirect
 from .models import *
 # Create your views here.
 
+add_notice_btn = {'url_name': 'add_notice'}
+
 def index(request):
     notice_card_information = Notice.objects.all()
     return render(request, 'notice/index.html', {'notice_card_information': notice_card_information})
 
-def fullNotice(request, notice_id):
-    return HttpResponse(f"<h1>FULL NOTICE</h1><p>{notice_id}</p>")
+def fullNotice(request):
+    return render(request,'notice/fullnotice.html')
+
 
 #exceptions
 def pageNotFound(request, exception):
